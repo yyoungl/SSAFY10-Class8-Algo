d58188724d8176ee5a8887023ee44baa8b460926
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-08-01 21:22:32 +0900

    Update yyoungl_20230801_P_소수_찾기.java

--- 20230802_P_소수 찾기/yyoungl_20230801_P_소수_찾기.java
+++ 20230802_P_소수 찾기/yyoungl_20230801_P_소수_찾기.java
@@ -14,7 +14,7 @@ public class yyoungl_20230801_P_소수_찾기 {
 
 		// 숫자를 1개 사용한 경우 ~ 모두 사용한 경우까지 만들 수 있는 숫자열 모두!!
 		for (int i = 0; i < numbers.length(); i++) {
-			dfs(numbers, "", i + 1);
+			dfs(numbers, "", i + 1); // 처음에 빈 문자열로 시
 		}
 
 		for (int i = 0; i < arr.size(); i++) {
@@ -36,9 +36,13 @@ public class yyoungl_20230801_P_소수_찾기 {
 		for (int i = 0; i < str.length(); i++) {
 			// 숫자가 사용되었는지, 사용되지 않았는지 확인하며 중복되지 않은 숫자로 만드는 숫자열 만들기
 			if (!check[i]) {
+				// 인덱스 i 에 해당하는 숫자를 사용하지 않았다면
 				check[i] = true;
 				temp += str.charAt(i);
+				// temp 에 숫자 i 이어붙이기
+				// 재귀 호출 코드, 
 				dfs(str, temp, m);
+				// str에 담긴 숫자들을 다시 temp ex) "1"에 이어붙이고, 숫자의 길이가 m이 될 때까지 반복
 				check[i] = false;
 				temp = temp.substring(0, temp.length() - 1);
 			}
