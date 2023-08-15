09d6144e687d95029f3ace0403701b75d9b96841
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-08-15 21:20:55 +0900

    스티    스티커 주

--- 20230816_B_9465/yyoungl_20230816_B_9465.java
+++ 20230816_B_9465/yyoungl_20230816_B_9465.java
@@ -23,6 +23,8 @@ public class yyoungl_20230816_B_9465 {
 			dp[1][1] = stickers[1][1];
 
 			for (int j = 2; j < N + 1; j++) {
+				// j 번째 스티커를 쓸 경우, 다른 행의 한 줄 앞과 두 줄 앞을 비교해서 더 큰 점수에 현재 칸 점수 더하기
+				// 원래 같은 행의 두 칸 앞도 비교에 넣어야 하나 생각했지만, 이건 앞열을 초기화할 때 이미 살펴보므로 필요 X
 				dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + stickers[0][j];
 				dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + stickers[1][j];
 
